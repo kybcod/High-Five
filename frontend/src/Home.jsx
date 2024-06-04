e216ce00b01b709f8eb260ecc4b2a961d6bb5104
kyb <kyb-0215@naver.com>
2024-06-04 17:30:44 +0900

    초기 세팅

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -2,7 +2,6 @@ import { Box } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 import { Navbar } from "./Navbar.jsx";
 import { Header } from "./Header.jsx";
-import { Category } from "./Category.jsx";
 
 export function Home() {
   return (
@@ -12,9 +11,6 @@ export function Home() {
       </Box>
       <Box>
         <Header />
-      </Box>
-      <Box>
-        <Category />
       </Box>
       <Box>
         <Outlet />
