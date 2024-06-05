4532e86ceca0abacfb7b9bf8ce7fb2ebbf5c1a79
kyb <kyb-0215@naver.com>
2024-06-05 23:34:14 +0900

    상품 조회 예시 css

--- frontend/src/component/Category.jsx
+++ frontend/src/component/Category.jsx
@@ -1,5 +1,17 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Center, Flex } from "@chakra-ui/react";
+import React from "react";
 
 export function Category() {
-  return <Box>카테고리</Box>;
+  return (
+    <Box bg="gray.200" p={4}>
+      <Flex justify="space-around">
+        <Center>의류</Center>
+        <Center>잡화</Center>
+        <Center>식품</Center>
+        <Center>디지털</Center>
+        <Center>스포츠</Center>
+        <Center>e-쿠폰</Center>
+      </Flex>
+    </Box>
+  );
 }
--- frontend/src/product/ProductList.jsx
+++ frontend/src/product/ProductList.jsx
@@ -1,5 +1,62 @@
-import { Box } from "@chakra-ui/react";
+import {
+  Box,
+  Card,
+  CardBody,
+  Center,
+  Flex,
+  Grid,
+  GridItem,
+  Heading,
+  Image,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import { Category } from "../component/Category.jsx";
 
 export function ProductList() {
-  return <Box>main</Box>;
+  // 샘플 데이터
+  const products = Array.from({ length: 20 }, (_, index) => ({
+    id: index + 1,
+    title: "제목",
+    inserted: `2024.06.03 12:00`,
+    price: `가격`,
+    imageUrl:
+      "https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+  }));
+
+  return (
+    <Box>
+      <Category />
+      <Center w="100%">
+        <Box h={"100px"} border={"1px solid black"}>
+          <Box>이미지 배너</Box>
+        </Box>
+      </Center>
+      <Heading my={4}>오늘의 상품</Heading>
+      <Grid templateColumns="repeat(5, 1fr)" gap={6}>
+        {products.map((product) => (
+          <GridItem key={product.id}>
+            <Card maxW="sm">
+              <CardBody>
+                <Image
+                  src={product.imageUrl}
+                  alt={product.title}
+                  borderRadius="lg"
+                />
+                <Stack mt="6" spacing="3">
+                  <Heading size="m">{product.title}</Heading>
+                  <Flex justifyContent={"space-between"}>
+                    <Text color="blue.600" fontSize="xl">
+                      {product.price}
+                    </Text>
+                    <Text>{product.inserted}</Text>
+                  </Flex>
+                </Stack>
+              </CardBody>
+            </Card>
+          </GridItem>
+        ))}
+      </Grid>
+    </Box>
+  );
 }
