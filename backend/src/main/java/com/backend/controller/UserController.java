d065be94a71fbe2b3eb449ea7391b33d5c75f250
jnn-jnn1 <anj72005@gmail.com>
2024-06-08 22:05:24 +0900

    인증번호 요청시 휴대폰 번호 쿼리스트링으로 전달

--- backend/src/main/java/com/backend/controller/UserController.java
+++ backend/src/main/java/com/backend/controller/UserController.java
@@ -15,19 +15,14 @@ public class UserController {
     @PostMapping("users")
     public void addUser(@RequestBody User user) {
 
-
         service.addUser(user);
     }
 
     // TODO. 나중에 활성화
     @GetMapping("users/code")
-    public void sendCode(@RequestBody User user) {
-
-        String phoneNumber = user.getPhoneNumber();
-        user.setPhoneNumber(phoneNumber);
-
-        String verificationCode = service.sendMessage(phoneNumber);
+    public void sendCode(String phoneNumber) {
+//        String verificationCode = service.sendMessage(phoneNumber);
         // TODO. 인증 확인 API 분리
-        service.checkVerificationCode(verificationCode, "");
+//        service.checkVerificationCode(verificationCode, "");
     }
 }
