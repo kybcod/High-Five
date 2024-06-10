091a44ab2349185e6fa8a4a46353280f434e5eee
Jin Jin <anj72005@gmail.com>
2024-06-10 12:20:45 +0900

    gradle update

---
+++ prj2-spring/src/test/java/com/prj2spring/Prj2SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prj2spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prj2SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
