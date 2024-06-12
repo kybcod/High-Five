74a95282c66bf38350f8e8dc9de8e093b47e2996
huitopia <huitopia27@gmail.com>
2024-06-12 10:16:46 +0900

    WebSocket 전송 설정

--- backend/src/main/java/com/backend/config/WebSocketConfiguration.java
+++ backend/src/main/java/com/backend/config/WebSocketConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -20,5 +21,12 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
         config.setApplicationDestinationPrefixes("/app");
         // SimpleBroker 위치는 SpringBoot 서버 내부 메모리에 존재
         config.enableSimpleBroker("/topic", "/queue");
+    }
+
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registry) {
+//        registry.setMessageSizeLimit(4 * 8192); // 메세지 크기 제한
+        registry.setSendTimeLimit(15 * 1000); // 메시지 전송 시간 제한 (예: 15초)
+        registry.setTimeToFirstMessage(30 * 1000); // 첫 번째 메세지 수신 시간 30초
     }
 }
