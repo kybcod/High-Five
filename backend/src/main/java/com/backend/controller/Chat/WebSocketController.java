3bdcabf012527efa6dbbd051bbda531eb8ff3a29
huitopia <huitopia27@gmail.com>
2024-06-12 12:10:38 +0900

    RequestBody 데이터 받기 test 완료

--- backend/src/main/java/com/backend/controller/Chat/WebSocketController.java
+++ backend/src/main/java/com/backend/controller/Chat/WebSocketController.java
@@ -1,21 +1,23 @@
 package com.backend.controller.Chat;
 
+import com.backend.domain.Chat.ChatMessage;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class WebSocketController {
     // WebSocket 메시지를 보내기 위한 간단한 템플릿을 제공
     private SimpMessagingTemplate template;
 
-    @MessageMapping("/chatroom/{roomId}") // /app/chatroom
+    @MessageMapping("/chatroom/{roomId}") // /app/chatroom/1
     @SendTo("/queue/{roomId}") // 대상자 정의
 //    @SubscribeMapping("") // 구독 메세지만
-    public String handle(@DestinationVariable String roomId, String str) {
-        System.out.println("str = " + str);
-        return str + str;
+    public String receiveMessage(@DestinationVariable Integer roomId, @RequestBody ChatMessage chatMessage) {
+        System.out.println("chatMessage = " + chatMessage);
+        return chatMessage.getMessage();
     }
 }
