9500aefd79d559d8d868994ed5020d18c7048195
huitopia <huitopia27@gmail.com>
2024-06-12 10:10:37 +0900

    Endpoint 지정

---
+++ backend/src/main/java/com/backend/config/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package com.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws");
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.setApplicationDestinationPrefixes("/app");
+        // SimpleBroker 위치는 SpringBoot 서버 내부 메모리에 존재
+        config.enableSimpleBroker("/topic", "/queue");
+    }
+}
