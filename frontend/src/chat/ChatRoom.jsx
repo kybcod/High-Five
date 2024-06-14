a33d070f8b7dec2bb234170d2cc564f578497b09
huitopia <huitopia27@gmail.com>
2024-06-14 18:50:38 +0900

    res.data 받기 완료

--- frontend/src/chat/ChatRoom.jsx
+++ frontend/src/chat/ChatRoom.jsx
@@ -1,20 +1,37 @@
-import { Box, Button, Heading, Input } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import { Box, Button, Heading, Input, Spinner } from "@chakra-ui/react";
+import { useContext, useEffect, useState } from "react";
 import * as StompJs from "@stomp/stompjs";
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { LoginContext } from "../component/LoginProvider.jsx";
 
 export function ChatRoom() {
-  const { product_id } = useParams();
+  const { productId } = useParams();
+  const account = useContext(LoginContext);
   // const chatroomId = param.chatroomId;
   const [chatList, setChatList] = useState([]); // 채팅 리스트
+  const [roomInfo, setRoomInfo] = useState(null);
+  const [roomId, setRoomId] = useState(null);
   const [chat, setChat] = useState(""); // 입력된 채팅 내용
   // -- GPT
   const [stompClient, setStompClient] = useState(null);
   const [message, setMessage] = useState("");
-  const roomId = 1;
   const userId = 1;
   const [messages, setMessages] = useState([]);
+  // -- axios.get
+  useEffect(() => {
+    axios
+      .get(`/api/chat/${productId}`)
+      .then((res) => {
+        console.log(res.data);
+        setRoomInfo(res.data);
+        setRoomId(res.data.id);
+      })
+      .catch()
+      .finally();
+  }, []);
 
+  // -- stomp
   useEffect(() => {
     const client = new StompJs.Client({
       brokerURL: "ws://localhost:8080/ws",
@@ -31,7 +48,9 @@ export function ChatRoom() {
       onConnect: function () {
         console.log("Connected to WebSocket");
         client.subscribe(`/user/queue/chat`, callback, { ack: "client" }); // 상대방
-        client.subscribe(`/topic/chat/${roomId}`, callback, { ack: "client" }); // 본인
+        client.subscribe(`/topic/chat/${roomInfo.id}`, callback, {
+          ack: "client",
+        }); // 본인
       },
       onStompError: (frame) => {
         console.error("STOMP error: ", frame);
@@ -79,12 +98,15 @@ export function ChatRoom() {
     stompClient.deactivate();
     console.log("Disconnected");
   };
+  if (roomInfo == null) {
+    return <Spinner />;
+  }
 
   return (
     <Box>
       <Box>
         <Box>
-          <Heading>Chat Room: {roomId}</Heading>
+          <Heading>No.{roomInfo.id} ChatRoom</Heading>
           <Box>
             {messages.map((msg, index) => (
               <Box key={index}>
@@ -99,7 +121,7 @@ export function ChatRoom() {
             value={message}
             onChange={(e) => setMessage(e.target.value)}
           />
-          <Button onClick={sendMessage}>Send</Button>
+          <Button onClick={sendMessage}>send</Button>
         </Box>
       </Box>
     </Box>
