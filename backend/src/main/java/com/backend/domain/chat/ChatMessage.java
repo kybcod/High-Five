12e272390aa7fa3307aaba42c6a7e322e6f7cacc
huitopia <huitopia27@gmail.com>
2024-06-15 03:12:07 +0900

    전송 시간 추가

--- backend/src/main/java/com/backend/domain/chat/ChatMessage.java
+++ backend/src/main/java/com/backend/domain/chat/ChatMessage.java
@@ -2,9 +2,12 @@ package com.backend.domain.chat;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class ChatMessage {
     private Integer chatRoomId;
     private Integer userId;
     private String message;
+    private LocalDateTime inserted;
 }
