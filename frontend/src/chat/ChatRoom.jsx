05cacaf0973ab4cca8f34eac82450023cad3f5a5
huitopia <huitopia27@gmail.com>
2024-06-15 00:39:36 +0900

    server data useState에 넣기

--- frontend/src/chat/ChatRoom.jsx
+++ frontend/src/chat/ChatRoom.jsx
@@ -1,9 +1,36 @@
-import { Box, Button, Heading, Input, Spinner } from "@chakra-ui/react";
+import { Box, Button, Flex, Input, Spinner } from "@chakra-ui/react";
 import { useContext, useEffect, useState } from "react";
 import * as StompJs from "@stomp/stompjs";
-import { useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 import { LoginContext } from "../component/LoginProvider.jsx";
+
+const ProductStateComp = ({ productInfo, userId }) => {
+  let productStateText = "";
+  if (productInfo.status == 0 && productInfo.buyerId == userId) {
+    // 후기 작성 모달 떠야함
+    productStateText = "거래완료";
+  } else if (productInfo.status == 1) {
+    // 상품 디테일 페이지로 이동
+    productStateText = "입찰가능";
+  } else {
+    // 버튼 readOnly
+    productStateText = "판매종료";
+  }
+  return (
+    <>
+      <Button colorScheme={"yellow"}>{productStateText}</Button>
+    </>
+  );
+};
+
+function ChatRoomDeleteComp() {
+  return (
+    <>
+      <Button colorScheme={"yellow"}>채팅삭제</Button>
+    </>
+  );
+}
 
 export function ChatRoom() {
   const { productId } = useParams();
@@ -11,6 +38,7 @@ export function ChatRoom() {
   // const chatroomId = param.chatroomId;
   const [chatList, setChatList] = useState([]); // 채팅 리스트
   const [roomInfo, setRoomInfo] = useState(null);
+  const [productInfo, setProductInfo] = useState(null);
   const [roomId, setRoomId] = useState(null);
   const [chat, setChat] = useState(""); // 입력된 채팅 내용
   // -- GPT
@@ -18,19 +46,19 @@ export function ChatRoom() {
   const [message, setMessage] = useState("");
   const userId = 1;
   const [messages, setMessages] = useState([]);
+  const navigate = useNavigate();
   // -- axios.get
   useEffect(() => {
     axios
       .get(`/api/chat/${productId}`)
       .then((res) => {
-        console.log(res.data);
-        setRoomInfo(res.data);
-        setRoomId(res.data.id);
+        setRoomInfo(res.data.chatRoom);
+        setProductInfo(res.data.chatProduct);
+        setRoomId(res.data.chatRoom.id);
       })
       .catch()
       .finally();
-  }, []);
-
+  }, [roomId]);
   // -- stomp
   useEffect(() => {
     const client = new StompJs.Client({
@@ -43,8 +71,8 @@ export function ChatRoom() {
         console.log(str);
       },
       reconnectDelay: 5000,
-      heartbeatIncoming: 30000,
-      heartbeatOutgoing: 30000,
+      heartbeatIncoming: 30 * 1000,
+      heartbeatOutgoing: 30 * 1000,
       onConnect: function () {
         console.log("Connected to WebSocket");
         client.subscribe(`/user/queue/chat`, callback, { ack: "client" }); // 상대방
@@ -65,7 +93,7 @@ export function ChatRoom() {
     //     disConnect();
     //   }
     // };
-  }, [roomId]);
+  }, []); // 로딩 속도 붙길래 걸어버림
 
   const callback = (message) => {
     const receivedMessage = JSON.parse(message.body);
@@ -76,8 +104,8 @@ export function ChatRoom() {
 
   const sendMessage = () => {
     let chatMessage = {
-      roomId,
-      userId,
+      chatRoomId: roomInfo.id,
+      userId: account.id,
       message: message,
     };
     console.log("send Message!");
@@ -87,9 +115,9 @@ export function ChatRoom() {
     });
 
     // -- 내가 보낸 거
-    // let formattedMessage = chatMessage;
-    // console.log("formattedMessage", formattedMessage);
-    // setMessages((prevMessages) => [...prevMessages, formattedMessage]);
+    let formattedMessage = chatMessage;
+    console.log("formattedMessage", formattedMessage);
+    setMessages((prevMessages) => [...prevMessages, formattedMessage]);
     setMessage("");
   };
 
@@ -105,13 +133,43 @@ export function ChatRoom() {
   return (
     <Box>
       <Box>
+        <Button colorScheme={"yellow"}>
+          {/* 경로 수정 : 채팅 목록*/}
+          ChatList
+        </Button>
+      </Box>
+      <Box>
+        <Flex>
+          <Box>
+            <Link to={`/product/${productInfo.id}`}>{productInfo.title}</Link>
+          </Box>
+          <Button
+            to={`/shop/${roomId.sellerId}/products`}
+            colorScheme={"yellow"}
+          >
+            {/* 경로 수정 예정 : 내 정보 확인 -> 닉네임 상점 */}
+            {roomInfo.sellerName} 상점
+          </Button>
+        </Flex>
+        <Flex>
+          <ProductStateComp
+            productInfo={productInfo}
+            userId={`${account.id}`}
+          />
+          <ChatRoomDeleteComp />
+        </Flex>
+      </Box>
+      <Box>
         <Box>
-          <Heading>No.{roomInfo.id} ChatRoom</Heading>
           <Box>
             {messages.map((msg, index) => (
               <Box key={index}>
                 <li>
-                  [{msg.roomId}] {msg.userId}: {msg.message}
+                  [{msg.chatRoomId}]{" "}
+                  {msg.userId == account.id
+                    ? roomInfo.userName
+                    : roomInfo.sellerName}
+                  : {msg.message}
                 </li>
               </Box>
             ))}
