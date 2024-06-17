85aacc407acc7fa3c45df2484aebce2175fc0bd9
Hwayeong <joid1004@naver.com>
2024-06-17 18:21:27 +0900

    comment 수정 화면 만드는 중

--- frontend/src/Question/Comment.jsx
+++ frontend/src/Question/Comment.jsx
@@ -2,10 +2,12 @@ import { Button, Flex, Input, Textarea, useToast } from "@chakra-ui/react";
 import React, { useContext, useEffect } from "react";
 import axios from "axios";
 import { LoginContext } from "../component/LoginProvider.jsx";
+import { CommentEdit } from "./CommentEdit.jsx";
 
-export function Comment({ comment, questionId }) {
+export function Comment({ comment }) {
   const account = useContext(LoginContext);
   const toast = useToast();
+
   // Prop이 제대로 넘어오는지 확인하기 위해 콘솔 로그 추가
   useEffect(() => {
     console.log("Comment prop:", comment);
@@ -20,25 +22,25 @@ export function Comment({ comment, questionId }) {
       .then(() => {
         toast({
           status: "success",
-          description: `${questionId}번 게시물이 삭제 되었습니다.`,
+          description: `댓글이 삭제 되었습니다.`,
           position: "bottom",
           duration: 2000,
         });
       })
       .catch((err) => {
         const code = err.response.status;
-        if (code === 404) {
-          toast({
-            status: "error",
-            description: `id가 없습니다.`,
-            position: "bottom",
-            duration: 2000,
-          });
-        }
         if (code === 403) {
           toast({
             status: "error",
             description: `삭제 권한이 없습니다`,
+            position: "bottom",
+            duration: 2000,
+          });
+        }
+        if (code === 404) {
+          toast({
+            status: "error",
+            description: `id가 없습니다.`,
             position: "bottom",
             duration: 2000,
           });
@@ -54,6 +56,11 @@ export function Comment({ comment, questionId }) {
       .finally();
   }
 
+  function handleModifyclick() {
+    console.log(`${comment.id}번 수정 버튼 클릭함`);
+    return <CommentEdit comment={comment} />;
+  }
+
   return (
     <>
       <Flex gap={3}>
@@ -62,7 +69,7 @@ export function Comment({ comment, questionId }) {
         <Input value={comment.inserted} />
         {account.hasAccess(comment.userId) && (
           <>
-            <Button>수정</Button>
+            <Button onClick={handleModifyclick}>수정</Button>
             <Button onClick={handleRemoveClick}>삭제</Button>
           </>
         )}
