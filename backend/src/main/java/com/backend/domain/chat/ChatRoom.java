b6138e32e12d2681d342b27c5e0a542b506d7b3d
huitopia <huitopia27@gmail.com>
2024-06-18 16:22:57 +0900

    chat_room db 컬럼과 동일하게 변경 및 getChatRoomIdAndBuyerId 메소드 추가

--- backend/src/main/java/com/backend/domain/chat/ChatRoom.java
+++ backend/src/main/java/com/backend/domain/chat/ChatRoom.java
@@ -3,14 +3,22 @@ package com.backend.domain.chat;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @Data
 public class ChatRoom {
     private Integer id;
     private Integer productId;
     private Integer sellerId;
-    private String sellerName;
     private Integer userId;
-    private String userName;
     private LocalDateTime inserted;
+
+    // -- chatList
+    public Map<String, Object> getChatRoomIdAndBuyerId() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", getId());
+        map.put("userId", getUserId());
+        return map;
+    }
 }
