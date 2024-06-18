ccb68c8936a3480f23ffa11fd9e52c236a220da0
huitopia <huitopia27@gmail.com>
2024-06-19 01:44:51 +0900

    selectChatProductInfo 반환 타입 변경 및 reivew_status 컬럼 추가 조회

--- backend/src/main/java/com/backend/mapper/product/ProductMapper.java
+++ backend/src/main/java/com/backend/mapper/product/ProductMapper.java
@@ -1,7 +1,6 @@
 package com.backend.mapper.product;
 
 import com.backend.domain.auction.BidList;
-import com.backend.domain.chat.ChatProduct;
 import com.backend.domain.chat.ChatRoom;
 import com.backend.domain.product.Product;
 import com.backend.domain.product.ProductWithUserDTO;
@@ -210,11 +209,11 @@ public interface ProductMapper {
     Integer selectProductSellerId(Integer productId);
 
     @Select("""
-            SELECT id, title, status
+            SELECT id, title, status, review_status
             FROM product
             WHERE id =#{productId}
             """)
-    ChatProduct selectChatProductInfo(Integer productId);
+    Product selectChatProductInfo(Integer productId);
 
     @Select("""
             SELECT b.user_id buyerId
