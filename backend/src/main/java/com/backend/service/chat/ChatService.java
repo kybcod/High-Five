529d90820f9e04b812b87764bac0c5ca5b56b20f
huitopia <huitopia27@gmail.com>
2024-06-18 16:25:54 +0900

    채팅 목록 조회 서비스 생성

--- backend/src/main/java/com/backend/service/chat/ChatService.java
+++ backend/src/main/java/com/backend/service/chat/ChatService.java
@@ -1,8 +1,11 @@
 package com.backend.service.chat;
 
+import com.backend.domain.chat.Chat;
 import com.backend.domain.chat.ChatMessage;
 import com.backend.domain.chat.ChatProduct;
 import com.backend.domain.chat.ChatRoom;
+import com.backend.domain.product.Product;
+import com.backend.domain.user.User;
 import com.backend.mapper.chat.ChatMapper;
 import com.backend.mapper.product.ProductMapper;
 import com.backend.mapper.user.UserMapper;
@@ -12,10 +15,8 @@ import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.time.LocalDateTime;
+import java.util.*;
 
 @Service
 @Transactional(rollbackFor = Exception.class)
@@ -25,7 +26,6 @@ public class ChatService {
     private final ChatMapper mapper;
     private final ProductMapper productMapper;
     private final UserMapper userMapper;
-    private final ChatMapper chatMapper;
 
     public Map<String, Object> selectChatRoomId(Integer productId, Authentication authentication) {
         Map<String, Object> result = new HashMap<>();
@@ -61,11 +61,11 @@ public class ChatService {
         ChatRoom chatRoom = mapper.selectChatRoomInfo(roomId);
 
         // userName 추가
-        chatRoom.setUserName(jwt.getClaim("nickName"));
+//        chatRoom.setUserName(jwt.getClaim("nickName"));
 
         // sellerName 추가
         String sellerName = userMapper.selectSellerName(chatRoom);
-        chatRoom.setSellerName(sellerName);
+//        chatRoom.setSellerName(sellerName);
 
         // -- chat product info
         // ChatProduct status = 1: 판매중, 0: 판매완료
@@ -85,8 +85,44 @@ public class ChatService {
         int success = mapper.insertMessage(chatMessage);
     }
 
-    public void selectChatList(Authentication authentication) {
-        Integer userId = Integer.valueOf(authentication.getName());
-        // TODO : mapper 작성
+    public List<Map<String, Object>> getChatRoomList(Authentication authentication) {
+        Integer tokenUserId = Integer.valueOf(authentication.getName());
+
+        List<Map<String, Object>> result = new ArrayList<>();
+        
+        List<ChatRoom> chatRoomList = mapper.selectChatRoomListByUserId(tokenUserId);
+        for (ChatRoom chatRoom : chatRoomList) {
+            Map<String, Object> map = new HashMap<>();
+
+            // -- product : id, title
+            Product product = productMapper.selectProductTitleById(chatRoom);
+
+            // -- 상대 user : id, nickName
+            Integer userId; // 상대방 아이디 얻기
+            if (chatRoom.getSellerId() == tokenUserId) { // 로그인 본인이 판매자면
+                // userId, name 얻기
+                userId = chatRoom.getUserId();
+            } else {
+                userId = chatRoom.getSellerId();
+            }
+            User user = userMapper.selectUserNickNameById(userId);
+
+            // -- chat : message, inserted
+            Chat chat = mapper.selectMessageByRoomId(chatRoom);
+            if (chat == null) {
+                Chat newChat = new Chat();
+                newChat.setMessage("대화를 시작해보세요!");
+                newChat.setInserted(LocalDateTime.now());
+                chat = newChat;
+            }
+
+            map.put("chatRoom", chatRoom.getChatRoomIdAndBuyerId());
+            map.put("chat", chat.getChatMessageAndInserted());
+            map.put("product", product.getProductIdAndTitle());
+            map.put("user", user.getUserIdAndNickName());
+
+            result.add(map);
+        }
+        return result;
     }
 }
