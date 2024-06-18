10525914c8dc450c9c6be3718d1c4585dce987bf
kyb <kyb-0215@naver.com>
2024-06-18 15:46:06 +0900

    쿼리 사용

--- backend/sql/product.sql
+++ backend/sql/product.sql
@@ -10,59 +10,21 @@ SELECT *
 FROM user;
 
 SELECT *
-FROM product
-WHERE id = 55;
-
-UPDATE product
-SET status = TRUE
-WHERE status = FALSE;
+FROM product;
 
 
-SELECT p.id,
-       p.title,
-       p.category,
-       p.start_price,
-       p.status,
-       p.content,
-       p.start_time,
-       p.end_time,
-       bl.status
-FROM product p
-         LEFT JOIN bid_list bl
-                   ON p.id = bl.product_id
-ORDER BY p.end_time;
+SELECT *
+FROM chat;
 
-
-SELECT MAX(bid_price)
-FROM bid_list
-WHERE product_id = 28;
-
-SELECT p.id,
-       p.user_id,
-       p.title,
-       p.category,
-       p.start_price,
-       p.start_time,
-       p.end_time,
-       p.content,
-       p.view_count,
-       p.status,
-       COUNT(DISTINCT bl.user_id) AS numberOfJoin,
-       u.nick_name                AS userNickName,
-       MAX(bl.bid_price)          AS maxBidPrice
-FROM product p
-         LEFT JOIN bid_list bl
-                   ON p.id = bl.product_id
-         JOIN user u ON u.id = p.user_id
-WHERE p.id = 28;
-
-
+SELECT *
+FROM chat_room;
 
 SHOW STATUS LIKE 'threads_connected'; # 148
-SHOW VARIABLES LIKE 'max_connections'; # 151
+SHOW VARIABLES LIKE 'max_connections'; # 300
 SHOW VARIABLES LIKE 'wait_timeout'; #28800
 
 SET GLOBAL MAX_CONNECTIONS = 300;
 
 
+DESC chat;
 DESC chat_room;
