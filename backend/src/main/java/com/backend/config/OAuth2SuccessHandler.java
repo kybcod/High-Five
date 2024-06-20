06499e50b1c9fb7eda8f57b6afa1f7d3f07bcd0e
jnn-jnn1 <anj72005@gmail.com>
2024-06-21 02:55:45 +0900

    authenticaiton 객체 기반으로 db 조회 후 토큰 발급

--- backend/src/main/java/com/backend/config/OAuth2SuccessHandler.java
+++ backend/src/main/java/com/backend/config/OAuth2SuccessHandler.java
@@ -3,15 +3,20 @@ package com.backend.config;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@RequiredArgsConstructor
+@Component
 public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
+    private final TokenProvider tokenProvider;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        
+        String token = tokenProvider.generateToken(authentication);
     }
 }
--- backend/src/main/java/com/backend/config/TokenProvider.java
+++ backend/src/main/java/com/backend/config/TokenProvider.java
@@ -1,14 +1,58 @@
 package com.backend.config;
 
+import com.backend.domain.user.CustomOauth2UserDetails;
+import com.backend.domain.user.User;
+import com.backend.mapper.user.UserMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Component;
 
+import java.time.Instant;
+import java.util.List;
+import java.util.Map;
+
+@Component
 @RequiredArgsConstructor
 public class TokenProvider {
     private final JwtEncoder jwtEncoder;
+    private final UserMapper userMapper;
 
-//    private String generateToken(Authentication authentication, long expireTime) {
-//        Instant now = Instant.now();
-//    }
+    public String generateToken(Authentication authentication) {
+
+        CustomOauth2UserDetails details = (CustomOauth2UserDetails) authentication.getPrincipal();
+        Map<String, Object> attributes = details.getAttributes();
+
+        User db = userMapper.selectUserByEmail(attributes.get("email").toString());
+        List<String> authorities = userMapper.selectAuthoritiesByUserId(db.getId());
+        String authorityString = "user";
+        if (authorities != null) {
+            db.setAuthority(authorities);
+            authorityString = db.getAuth();
+        }
+
+        String profileImage = "";
+        String fileName = userMapper.selectFileNameByUserId(db.getId());
+        if (fileName != null) {
+            profileImage = STR."https://study34980.s3.ap-northeast-2.amazonaws.com/prj3/user/\{db.getId()}/\{fileName}";
+        }
+
+        Instant now = Instant.now();
+
+        JwtClaimsSet jwtClaimsSet = JwtClaimsSet.builder()
+                .issuer("LiveAuction")
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(60 * 60 * 24 * 7))
+                .subject(db.getId().toString())
+                .claim("nickName", db.getNickName())
+                .claim("scope", authorityString)
+                .claim("email", db.getEmail())
+                .claim("profileImage", profileImage)
+                .build();
+
+        return jwtEncoder.encode(JwtEncoderParameters.from(jwtClaimsSet)).getTokenValue();
+    }
 
 }
