859789c99a2b1a8e0dd8c2fbeb0f7e43bab1651d
Hwayeong <joid1004@naver.com>
2024-06-23 19:04:25 +0900

    게시글이 길면 맨 위로 버튼 생성 버튼을 눌렀을 때 화면 맨 위로 스크롤

--- frontend/src/Question/CommentList.jsx
+++ frontend/src/Question/CommentList.jsx
@@ -1,14 +1,9 @@
 import axios from "axios";
-import {
-  Box,
-  Card,
-  CardBody,
-  Stack,
-  StackDivider,
-  useToast,
-} from "@chakra-ui/react";
-import React, { useContext, useEffect, useState } from "react";
+import { Box, Card, CardBody, Stack, StackDivider } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
 import { Comment } from "./Comment.jsx";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAnglesUp } from "@fortawesome/free-solid-svg-icons";
 
 export function CommentList({ questionId }) {
   const [commentList, setCommentList] = useState([]);
@@ -25,6 +20,43 @@ export function CommentList({ questionId }) {
     }
   }, []);
 
+  const [isVisible, setIsVisible] = useState(false);
+
+  // 스크롤 이벤트 핸들러를 설정합니다.
+  const toggleVisibility = () => {
+    if (window.scrollY > 500) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  // 화면 맨 위로 스크롤합니다.
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  // 컴포넌트가 마운트될 때와 언마운트될 때 스크롤 이벤트를 등록하고 해제합니다.
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
+    };
+  }, []);
+
+  const buttonStyle = {
+    position: "fixed",
+    bottom: "20px",
+    right: "100px",
+    zIndex: 1000,
+    cursor: "pointer",
+    transition: "opacity 0.3s, visibility 0.3s",
+    display: isVisible ? "block" : "none",
+  };
+
   return (
     <Box>
       {commentList.length > 0 && (
@@ -37,6 +69,11 @@ export function CommentList({ questionId }) {
             </Stack>
           </CardBody>
         </Card>
+      )}
+      {isVisible && (
+        <Box onClick={scrollToTop} style={buttonStyle}>
+          <FontAwesomeIcon icon={faAnglesUp} size="2xl" />
+        </Box>
       )}
     </Box>
   );
