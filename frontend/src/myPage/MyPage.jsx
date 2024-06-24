b8cf9a8b08fb7c72167b13e735b2b6c655bbbd02
kyb <kyb-0215@naver.com>
2024-06-24 16:48:30 +0900

    탭 순서 수정

--- frontend/src/myPage/MyPage.jsx
+++ frontend/src/myPage/MyPage.jsx
@@ -8,7 +8,7 @@ import {
   Tabs,
 } from "@chakra-ui/react";
 import { useContext, useEffect, useState } from "react";
-import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { MyShop } from "./MyShop.jsx";
 import { LikeList } from "./LikeList.jsx";
 import axios from "axios";
@@ -19,40 +19,32 @@ import { LoginContext } from "../component/LoginProvider.jsx";
 export function MyPage({ tab }) {
   const [userNickName, setUserNickName] = useState("");
   const navigate = useNavigate();
-  const location = useLocation();
   const { userId } = useParams();
   const account = useContext(LoginContext);
 
   // 탭 이름 -> 인덱스
   const tabIndex = {
-    userInfo: 0,
-    like: 1,
-    shop: 2,
-    bids: 3,
-    reviews: 4,
+    shop: 0,
+    likes: 1,
+    bids: 2,
+    userInfo: 3,
   };
 
   // 인덱스 -> 택이름
   const indexTab = {
-    0: "userInfo",
-    1: "like",
-    2: "shop",
-    3: "bids",
-    4: "reviews",
+    0: "shop",
+    1: "likes",
+    2: "bids",
+    3: "userInfo",
   };
 
-  const currentTab = tab || "userInfo";
+  const currentTab = tab;
 
   useEffect(() => {
     axios.get(`/api/products/user/${userId}`).then((res) => {
       setUserNickName(res.data.userNickName);
     });
-
-    const tabName = location.pathname.split("/").pop();
-    if (!Object.keys(tabIndex).includes(tabName)) {
-      navigate(`/myPage/${userId}`);
-    }
-  }, [location.pathname]);
+  }, []);
 
   const handleTabsChange = (index) => {
     navigate(`/myPage/${userId}/${indexTab[index]}`);
@@ -70,24 +62,6 @@ export function MyPage({ tab }) {
         onChange={handleTabsChange}
       >
         <TabList w={"30%"} m={3} mr={20}>
-          {userId == account.id && (
-            <>
-              <Tab
-                border={"1px solid #eee"}
-                borderRadius="3px"
-                _selected={{ borderColor: "green.500", fontWeight: "bold" }}
-              >
-                내 정보 확인
-              </Tab>
-              <Tab
-                border={"1px solid #eee"}
-                borderRadius="3px"
-                _selected={{ borderColor: "green.500", fontWeight: "bold" }}
-              >
-                찜 목록
-              </Tab>
-            </>
-          )}
           <Tab
             border={"1px solid #eee"}
             borderRadius="3px"
@@ -96,37 +70,32 @@ export function MyPage({ tab }) {
             판매 목록
           </Tab>
           {userId == account.id && (
-            <Tab
-              border={"1px solid #eee"}
-              borderRadius="3px"
-              _selected={{ borderColor: "green.500", fontWeight: "bold" }}
-            >
-              입찰 내역
-            </Tab>
+            <>
+              <Tab
+                border={"1px solid #eee"}
+                borderRadius="3px"
+                _selected={{ borderColor: "green.500", fontWeight: "bold" }}
+              >
+                찜 목록
+              </Tab>
+              <Tab
+                border={"1px solid #eee"}
+                borderRadius="3px"
+                _selected={{ borderColor: "green.500", fontWeight: "bold" }}
+              >
+                입찰 내역
+              </Tab>
+              <Tab
+                border={"1px solid #eee"}
+                borderRadius="3px"
+                _selected={{ borderColor: "green.500", fontWeight: "bold" }}
+              >
+                내 정보
+              </Tab>
+            </>
           )}
-          <Tab
-            border={"1px solid #eee"}
-            borderRadius="3px"
-            _selected={{ borderColor: "green.500", fontWeight: "bold" }}
-          >
-            받은 후기
-          </Tab>
         </TabList>
         <TabPanels>
-          {userId == account.id && (
-            <TabPanel>
-              <Box>
-                <UserInfo />
-              </Box>
-            </TabPanel>
-          )}
-          {userId == account.id && (
-            <TabPanel>
-              <Box>
-                <LikeList />
-              </Box>
-            </TabPanel>
-          )}
           <TabPanel>
             <Box>
               <MyShop />
@@ -135,13 +104,24 @@ export function MyPage({ tab }) {
           {userId == account.id && (
             <TabPanel>
               <Box>
+                <LikeList />
+              </Box>
+            </TabPanel>
+          )}
+          {userId == account.id && (
+            <TabPanel>
+              <Box>
                 <BidList />
               </Box>
             </TabPanel>
           )}
-          <TabPanel>
-            <Box>받은 후기</Box>
-          </TabPanel>
+          {userId == account.id && (
+            <TabPanel>
+              <Box>
+                <UserInfo />
+              </Box>
+            </TabPanel>
+          )}
         </TabPanels>
       </Tabs>
     </Box>
