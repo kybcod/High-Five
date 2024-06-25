ba773248ba5cd1367c8683c01d8578b6069cbbd5
kyb <kyb-0215@naver.com>
2024-06-25 15:55:08 +0900

    판매하기 및 경매톡

--- frontend/src/component/Header.jsx
+++ frontend/src/component/Header.jsx
@@ -1,19 +1,19 @@
 import {
   Box,
   Button,
-  Center,
   Flex,
   Heading,
   Input,
   InputGroup,
   InputRightAddon,
-  Spacer,
+  Stack,
 } from "@chakra-ui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faComments,
-  faMagnifyingGlass,
-} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+  faSackDollar,
+  faSearch,
+} from "@fortawesome/free-solid-svg-icons"; // FontAwesome에서 추가 아이콘 가져오기
 import { useContext, useEffect, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { LoginContext } from "./LoginProvider.jsx";
@@ -25,9 +25,9 @@ export function Header() {
 
   const [searchParams] = useSearchParams();
   useEffect(() => {
-    const keyword1 = searchParams.get("keyword");
-    if (keyword1) {
-      setKeyword(keyword1);
+    const keywordFromParams = searchParams.get("keyword");
+    if (keywordFromParams) {
+      setKeyword(keywordFromParams);
     } else {
       setKeyword("");
     }
@@ -38,54 +38,60 @@ export function Header() {
   }
 
   return (
-    <Box>
-      <Flex mb={7} gap={3}>
-        <Center>
-          <Heading size="2xl">Live Auction</Heading>
-        </Center>
-        <Spacer />
-        <Center>
-          <Box>
-            <Flex>
-              <Box>
-                <InputGroup>
-                  <Input
-                    w={"300px"}
-                    placeholder={"검색어를 입력하세요."}
-                    value={keyword}
-                    onChange={(e) => setKeyword(e.target.value)}
-                  />
-                  <InputRightAddon>
-                    <Button
-                      onClick={() => handleSearchClick(keyword)}
-                      size={"100%"}
-                    >
-                      <FontAwesomeIcon icon={faMagnifyingGlass} />
-                    </Button>
-                  </InputRightAddon>
-                </InputGroup>
-              </Box>
-              <Box gap={1}></Box>
-            </Flex>
-          </Box>
-        </Center>
-        <Spacer />
-        <Center>
-          <Box>
-            {account.isLoggedIn() && (
-              <Center onClick={() => navigate("/write")} cursor={"pointer"}>
-                판매하기
-              </Center>
-            )}
-          </Box>
-        </Center>
-        <Center>
+    <Box py={4}>
+      <Flex align="center" justify="space-between" maxW="100%" mx="auto">
+        {/* 로고 */}
+        <Box>
+          <Heading cursor={"pointer"} onClick={() => navigate("/")}>
+            LIVE AUCTION{" "}
+          </Heading>
+        </Box>
+
+        {/* 검색 */}
+        <Flex align={"center"}>
+          <InputGroup>
+            <Input
+              w="500px"
+              placeholder="상품명 입력"
+              value={keyword}
+              onChange={(e) => setKeyword(e.target.value)}
+            />
+            <InputRightAddon
+              // bgColor={"mediumslateblue"}
+              onClick={() => handleSearchClick(keyword)}
+            >
+              <FontAwesomeIcon icon={faSearch} />
+            </InputRightAddon>
+          </InputGroup>
+        </Flex>
+
+        {/* 판매하기, 경매톡 */}
+        <Stack direction="row" spacing={2}>
           {account.isLoggedIn() && (
-            <Box onClick={() => navigate("/chat/list")} cursor={"pointer"}>
-              <FontAwesomeIcon icon={faComments} />
-            </Box>
+            <>
+              <Button
+                onClick={() => navigate("/write")}
+                size="md"
+                ml={4}
+                leftIcon={<FontAwesomeIcon icon={faSackDollar} />}
+                colorScheme="pink"
+                variant="unstyled"
+              >
+                판매하기
+              </Button>
+              <Button
+                onClick={() => navigate("/chat/list")}
+                size="md"
+                ml={4}
+                leftIcon={<FontAwesomeIcon icon={faComments} />}
+                colorScheme="blue"
+                variant="unstyled"
+              >
+                경매톡
+              </Button>
+            </>
           )}
-        </Center>
+        </Stack>
       </Flex>
     </Box>
   );
