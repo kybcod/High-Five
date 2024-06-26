3ef6978e2d2261863c644d2a88697bead71251ec
kyb <kyb-0215@naver.com>
2024-06-26 21:18:07 +0900

    정렬 수정

--- backend/src/main/java/com/backend/mapper/auction/AuctionMapper.java
+++ backend/src/main/java/com/backend/mapper/auction/AuctionMapper.java
@@ -38,7 +38,7 @@ public interface AuctionMapper {
             FROM bid_list b
                      JOIN product p ON b.product_id = p.id
             WHERE b.user_id = #{userId}
-            ORDER BY b.updated DESC, p.end_time
+            ORDER BY b.updated DESC, p.end_time, p.id
             LIMIT #{pageable.pageSize} OFFSET #{pageable.offset}
             """)
     @Results(id = "productBidList", value = {
