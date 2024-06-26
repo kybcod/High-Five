37c8954d67d23fe1ef740c95f05b40d2c75468ff
kyb <kyb-0215@naver.com>
2024-06-26 12:17:22 +0900

    카테고리 옵션

--- frontend/src/product/componentStyle/FormFields.jsx
+++ frontend/src/product/componentStyle/FormFields.jsx
@@ -42,7 +42,7 @@ export function FormFields({
   return (
     <Box>
       <Box mb={10} display="flex">
-        <FormLabel width="120px" pr={4} alignSelf="flex-start">
+        <FormLabel width="180px" pr={4} alignSelf="flex-start">
           카테고리
         </FormLabel>
         <FormControl flex="1">
@@ -106,7 +106,7 @@ export function FormFields({
         </FormControl>
       </Box>
       <Box mb={10} display="flex">
-        <FormLabel width="120px" pr={4} alignSelf="flex-start">
+        <FormLabel width="180px" pr={4} alignSelf="flex-start">
           상품명
         </FormLabel>
         <FormControl flex="1">
@@ -118,7 +118,7 @@ export function FormFields({
         </FormControl>
       </Box>
       <Box mb={10} display="flex">
-        <FormLabel width="120px" pr={4} alignSelf="flex-start">
+        <FormLabel width="180px" pr={4} alignSelf="flex-start">
           입찰 시작가
         </FormLabel>
         <FormControl flex="1">
@@ -133,7 +133,7 @@ export function FormFields({
         </FormControl>
       </Box>
       <Box mb={10} display="flex">
-        <FormLabel width="120px" pr={4} alignSelf="flex-start">
+        <FormLabel width="180px" pr={4} alignSelf="flex-start">
           경매 마감 시간
         </FormLabel>
         <FormControl flex="1">
@@ -157,13 +157,28 @@ export function FormFields({
               borderRadius="md"
               _focus={{ borderColor: "blue.500" }}
             >
-              {/* Options omitted for brevity */}
+              <option value="08:00">08:00</option>
+              <option value="09:00">09:00</option>
+              <option value="10:00">10:00</option>
+              <option value="11:00">11:00</option>
+              <option value="12:00">12:00</option>
+              <option value="13:00">13:00</option>
+              <option value="14:00">14:00</option>
+              <option value="15:00">15:00</option>
+              <option value="16:00">16:00</option>
+              <option value="17:00">17:00</option>
+              <option value="18:00">18:00</option>
+              <option value="19:00">19:00</option>
+              <option value="20:00">20:00</option>
+              <option value="21:00">21:00</option>
+              <option value="22:00">22:00</option>
+              <option value="23:00">23:00</option>
             </Select>
           </Flex>
         </FormControl>
       </Box>
       <Box mb={10} display="flex">
-        <FormLabel width="120px" pr={4} alignSelf="flex-start">
+        <FormLabel width="180px" pr={4} alignSelf="flex-start">
           상품 상세내용 (선택)
         </FormLabel>
         <FormControl flex="1">
