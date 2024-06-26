2fd512703ea5f3bf4b7d67b766a9046275c08683
jeongyun <zoe1126@naver.com>
2024-06-26 09:45:52 +0900

    프로필 이미지 없으면 기본이미지, 있으면 유저 파일 이름 출력

--- frontend/src/board/BoardView.jsx
+++ frontend/src/board/BoardView.jsx
@@ -118,12 +118,23 @@ export function BoardView() {
         <Box>
           <Text>{board.nickName}</Text>
         </Box>
-        <Image
-          fallbackSrc="https://study34980.s3.ap-northeast-2.amazonaws.com/prj3/profile/original_profile.jpg"
-          borderRadius="full"
-          boxSize="150px"
-          src={board.profileImage}
-        />
+        {board.profileImage && board.profileImage.fileName == null && (
+          <Image
+            borderRadius="full"
+            boxSize="150px"
+            fallbackSrc="https://mblogthumb-phinf.pstatic.net/MjAyMDExMDFfMTgy/MDAxNjA0MjI4ODc1NDMw.Ex906Mv9nnPEZGCh4SREknadZvzMO8LyDzGOHMKPdwAg.ZAmE6pU5lhEdeOUsPdxg8-gOuZrq_ipJ5VhqaViubI4g.JPEG.gambasg/%EC%9C%A0%ED%8A%9C%EB%B8%8C_%EA%B8%B0%EB%B3%B8%ED%94%84%EB%A1%9C%ED%95%84_%ED%95%98%EB%8A%98%EC%83%89.jpg?type=w800"
+            src={board.profileImage.src}
+            alt="Default Profile Image"
+          />
+        )}
+        {board.profileImage && board.profileImage.fileName != null && (
+          <Image
+            borderRadius="full"
+            boxSize="150px"
+            src={board.profileImage.src}
+            alt="Profile Image"
+          />
+        )}
         <Spacer />
         <Box>
           <Text>{board.inserted}</Text>
