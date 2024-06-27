74c755ffa1c86fca8f17b64441e1a586ac560a9d
huitopia <huitopia27@gmail.com>
2024-06-27 15:08:24 +0900

    deleteChatRoom 생성

--- backend/src/main/java/com/backend/controller/chat/ChatController.java
+++ backend/src/main/java/com/backend/controller/chat/ChatController.java
@@ -2,13 +2,11 @@ package com.backend.controller.chat;
 
 import com.backend.service.chat.ChatService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -18,6 +16,7 @@ import java.util.Map;
 @RequestMapping("/api/chats")
 public class ChatController {
     private final ChatService service;
+    private final ChatService chatService;
 
     ///api/chat/${productId}?userId=${account.id}
     // 채팅방 첫 입장 시 룸 정보 조회
@@ -25,7 +24,6 @@ public class ChatController {
     @PreAuthorize("isAuthenticated()")
     public ResponseEntity getChatRoomInfo(
             @PathVariable Integer productId, @PathVariable Integer buyerId, Authentication authentication) {
-
         // TODO : 로직 전부 변경 예정 후 status 추가
         Map<String, Object> result = service.selectChatRoomId(productId, buyerId, authentication);
         return ResponseEntity.ok().body(result);
@@ -36,6 +34,19 @@ public class ChatController {
     public ResponseEntity getChatList(Authentication authentication) {
         // TODO : status 추가 예정
         List<Map<String, Object>> result = service.getChatRoomList(authentication);
-        return ResponseEntity.ok().body(result);
+        if (result.size() > 0) {
+            return ResponseEntity.ok().body(result);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @DeleteMapping("{chatRoomId}")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity deleteChatRoom(@PathVariable Integer chatRoomId, Authentication authentication) {
+        Integer success = service.deleteChatRoomById(chatRoomId, authentication);
+        if (success != 1) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok().build();
     }
 }
