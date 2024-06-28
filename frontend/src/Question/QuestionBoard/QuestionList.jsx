e2c9b1e7d46568d93b8124de9321d63a97388f8f
Hwayeong <joid1004@naver.com>
2024-06-29 02:31:12 +0900

    비밀글 아이콘 수정 중

--- frontend/src/Question/QuestionBoard/QuestionList.jsx
+++ frontend/src/Question/QuestionBoard/QuestionList.jsx
@@ -4,6 +4,7 @@ import {
   Button,
   Center,
   Flex,
+  Image,
   Input,
   Select,
   Table,
@@ -119,15 +120,30 @@ export function QuestionList() {
                     <Td width="42%">
                       <Flex gap={2}>
                         {question.secretWrite && (
-                          <Box
-                            style={{ display: "flex", alignItems: "center" }}
-                          >
+                          <Flex gap={3}>
+                            <Image src={"/img/lock.svg"} />
+                            {/*<svg*/}
+                            {/*  xmlns="http://www.w3.org/2000/svg"*/}
+                            {/*  className="h-6 w-6"*/}
+                            {/*  fill="none"*/}
+                            {/*  viewBox="0 0 24 24"*/}
+                            {/*  stroke="currentColor"*/}
+                            {/*  strokeWidth={2}*/}
+                            {/*>*/}
+                            {/*  <path*/}
+                            {/*    strokeLinecap="round"*/}
+                            {/*    strokeLinejoin="round"*/}
+                            {/*    d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"*/}
+                            {/*  />*/}
+                            {/*</svg>*/}
                             <FontAwesomeIcon
                               icon={faLock}
                               style={{ marginRight: "4px" }}
                             />
-                            <span style={{ color: "gray" }}>비밀글</span>
-                          </Box>
+                            <span style={{ color: "gray", fontSize: "14px" }}>
+                              비밀글
+                            </span>
+                          </Flex>
                         )}
                         {question.secretWrite || question.title}
                         {question.isNewBadge && (
