e8fe35967c5192bf856c3621094b7a5525d4fad7
kyb <kyb-0215@naver.com>
2024-06-29 18:20:06 +0900

    슬라이더 화살표 버튼 컴포넌트

---
+++ frontend/src/component/SampleNextArrow.jsx
@@ -0,0 +1,29 @@
+import { Box } from "@chakra-ui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+
+export const SampleNextArrow = ({ onClick }) => {
+  return (
+    <Box
+      width={"50px"}
+      height={"50px"}
+      bgColor={"white"}
+      borderRadius="50%"
+      position="absolute"
+      transform="translateY(-50%)"
+      top="50%"
+      right="10px"
+      color="#06A458"
+      fontSize="30px"
+      zIndex="1"
+      cursor="pointer"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      onClick={onClick}
+    >
+      <FontAwesomeIcon icon={faChevronRight} size={"lg"} />
+    </Box>
+  );
+};
