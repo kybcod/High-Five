15764e1de23cd233fa5d6530318187430a2edaf0
jeongyun <zoe1126@naver.com>
2024-07-01 00:59:50 +0900

    대댓글 순서 (답글을 단 위치에서 답글을 보여주기), 카드 border 넘기지 않도록 설정
    comment_id에 따라 정렬

--- backend/sql/board.sql
+++ backend/sql/board.sql
@@ -1,5 +1,45 @@
 SELECT *
+FROM board
+order by id desc;
+
+SELECT MAX(ID) MAXID
+     , MIN(ID) MIDID
 FROM board;
+
+SELECT b.id,
+       b.title,
+       b.user_id,
+       u.nick_name nickName,
+       b.inserted,
+       b.content,
+       MAX(b.id)   maxId,
+       MIN(b.id)   minId
+FROM board b
+         JOIN user u ON b.user_id = u.id
+WHERE b.id;
+
+SELECT b.id,
+       b.title,
+       b.user_id,
+       u.nick_name                 nickName,
+       b.inserted,
+       b.content,
+       (select MAX(id) from board) maxId,
+       (select MIN(id) from board) minId
+FROM board b
+         JOIN user u ON b.user_id = u.id
+WHERE b.id = 108;
+
+select *
+from board
+where id > 101
+LIMIT 1;
+
+select *
+from board
+where id < 103
+ORDER BY ID desc
+LIMIT 1;
 
 SELECT *
 FROM board_like;
@@ -52,7 +92,7 @@ FROM board_comment;
 
 SELECT *
 FROM board_comment
-WHERE board_id = 86;
+WHERE board_id = 107;
 
 DELETE
 FROM board_comment
@@ -67,4 +107,8 @@ WHERE black_count > 0;
 
 ALTER TABLE board
     MODIFY COLUMN id INT AUTO_INCREMENT;
+
+# 보드 아이디를 순서대로, 수정삭제해도 아이디 오름차순 설정 위해 임시 아이디
+ALTER TABLE board
+    ADD COLUMN board_no INT;
 
