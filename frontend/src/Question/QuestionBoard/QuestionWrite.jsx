bf77539371d091317c15dfabae92285727bcd945
Hwayeong <joid1004@naver.com>
2024-06-30 22:34:46 +0900

    문의게시판 글쓰기 화면 css

--- frontend/src/Question/QuestionBoard/QuestionWrite.jsx
+++ frontend/src/Question/QuestionBoard/QuestionWrite.jsx
@@ -16,6 +16,11 @@ import {
   StackDivider,
   Text,
   Textarea,
+  Divider,
+  Table,
+  Tr,
+  Td,
+  Flex,
 } from "@chakra-ui/react";
 import React, { useContext, useEffect, useState } from "react";
 import axios from "axios";
@@ -81,81 +86,110 @@ export function QuestionWrite() {
   }
 
   return (
-    <Box m={8}>
-      <Box mt={5}>
-        <Heading>문의 글 작성</Heading>
-      </Box>
+    <>
+      <Text
+        mx="auto"
+        maxWidth="1000px"
+        fontSize={"2xl"}
+        fontWeight={"bold"}
+        mb={4}
+      >
+        1:1 문의
+      </Text>
+      <Divider border={"1px solid black"} mx="auto" maxWidth="1000px" my={4} />
 
-      <Box mt={2}>
-        <FormControl>
-          <FormLabel>제목</FormLabel>
-          <Input
-            onChange={(e) => setTitle(e.target.value)}
-            placeholder="내용을 입력해주세요"
-          ></Input>
-        </FormControl>
-      </Box>
-      <Checkbox isChecked={secretWrite} onChange={handlesecretWrite} mt={2}>
-        비밀글
-      </Checkbox>
+      <Box maxWidth="1000px" mx="auto" position="relative">
+        <Table align={"center"} fontSize={"15px"}>
+          <Tr>
+            <Td w={"15%"} fontWeight={700}>
+              옵션
+            </Td>
+            <Td>
+              <Checkbox
+                mr={2}
+                isChecked={secretWrite}
+                onChange={handlesecretWrite}
+              />
+              비밀글
+            </Td>
+          </Tr>
+          <Tr>
+            <Td w={"15%"} fontWeight={700}>
+              제목
+            </Td>
+            <Td>
+              <Input
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder="제목을 입력해주세요"
+              />
+            </Td>
+          </Tr>
+          <Tr>
+            <Td w={"15%"} fontWeight={700}>
+              내용
+            </Td>
+            <Td>
+              <Textarea
+                onChange={(e) => setContent(e.target.value)}
+                placeholder="내용을 입력해주세요"
+                maxLength="5000"
+              />
+            </Td>
+          </Tr>
+          <Tr>
+            <Td w={"15%"} fontWeight={700}>
+              파일 첨부
+            </Td>
+            <Td>
+              <FormControl mb={5}>
+                <Input
+                  multiple
+                  type="file"
+                  accept="image/*"
+                  onChange={handleInsertFiles}
+                />
+                <FormHelperText>
+                  이미지 파일만 업로드할 수 있습니다.
+                  <br />한 파일당 1MB를 초과할 수 없으며 총 용량은 10MB입니다.
+                </FormHelperText>
+              </FormControl>
+              {fileNameList.length > 0 && (
+                <Box mb={7}>
+                  <Card>
+                    <CardHeader>
+                      <Heading size="md">선택된 파일 목록</Heading>
+                    </CardHeader>
+                    <CardBody>
+                      <Stack divider={<StackDivider />} spacing={4}>
+                        {previewList.map((src, index) => (
+                          <Box key={index} mb={3}>
+                            {fileNameList[index]}
+                            <Image src={src} blockSize="300px" />
+                          </Box>
+                        ))}
+                      </Stack>
+                    </CardBody>
+                  </Card>
+                </Box>
+              )}
+            </Td>
+          </Tr>
+        </Table>
 
-      <Box mt={5}>
-        <FormControl>
-          <FormLabel>문의 상세내용</FormLabel>
-          <Textarea
-            onChange={(e) => setContent(e.target.value)}
-            placeholder="내용을 입력해주세요"
-          ></Textarea>
-        </FormControl>
+        <Flex justify="flex-end" position="absolute" right="0" bottom="-60px">
+          <Button
+            onClick={handleSaveClick}
+            isLoading={loading}
+            isDisabled={isDisableSaveButton}
+            colorScheme={"teal"}
+            w={120}
+            variant={"outline"}
+            borderRadius={"unset"}
+          >
+            저장하기
+          </Button>
+        </Flex>
       </Box>
-
-      <Box mb={7}>
-        <FormControl>
-          <FormLabel>파일</FormLabel>
-          <Input
-            multiple
-            type="file"
-            accept="image/*"
-            onChange={handleInsertFiles}
-          />
-          <FormHelperText>
-            이미지 파일만 업로드할 수 있습니다.
-            <br />총 용량은 10MB이며, 한 파일은 1MB를 초과할 수 없습니다.
-          </FormHelperText>
-        </FormControl>
-      </Box>
-      {fileNameList.length > 0 && (
-        <Box mb={7}>
-          <Card>
-            <CardHeader>
-              <Heading size="md">선택된 파일 목록</Heading>
-            </CardHeader>
-            <CardBody>
-              <Stack divider={<StackDivider />} spacing={4}>
-                {previewList.map((src, index) => (
-                  <Box key={index} mb={3}>
-                    {fileNameList[index]}
-                    <Image src={src} blockSize="300px" />
-                  </Box>
-                ))}
-              </Stack>
-            </CardBody>
-          </Card>
-        </Box>
-      )}
-
-      <Center>
-        <Button
-          onClick={handleSaveClick}
-          isLoading={loading}
-          isDisabled={isDisableSaveButton}
-          colorScheme={"blue"}
-          mt={3}
-          w={500}
-        >
-          저장하기
-        </Button>
-      </Center>
-    </Box>
+    </>
   );
 }
